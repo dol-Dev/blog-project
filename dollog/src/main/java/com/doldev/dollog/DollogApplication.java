6ecfd1cbf716c6ad0fe48e44a4d866af982c8f1b
dol-Dev <dol.coder@gmail.com>
2025-02-03 20:48:22 +0900

    init: 프로젝트 초기 설정 및 기본 파일 구성
    - application.properties 및 application-dev.properties 파일 생성
    - build.gradle 설정
    - 기본 패키지 구조 생성

---
+++ dollog/src/main/java/com/doldev/dollog/DollogApplication.java
@@ -0,0 +1,13 @@
+package com.doldev.dollog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollogApplication.class, args);
+	}
+
+}
---
+++ dollog/src/test/java/com/doldev/dollog/DollogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doldev.dollog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
