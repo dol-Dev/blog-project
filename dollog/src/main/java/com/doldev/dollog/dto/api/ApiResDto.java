16a50111c4896ffbee5a0282dde0d861222ba6c7
dol-Dev <dol.coder@gmail.com>
2025-02-05 04:40:45 +0900

    feat: add ApiResDto for standardized response format

---
+++ dollog/src/main/java/com/doldev/dollog/dto/api/ApiResDto.java
@@ -0,0 +1,19 @@
+package com.doldev.dollog.dto.api;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResDto<T> {
+    private String messageCode;
+    private T data;
+    private Map<String, String> errors;
+}
