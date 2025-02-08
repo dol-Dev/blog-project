afc107ad05a921f070364b4d8755ce51524fd165
dol-Dev <dol.coder@gmail.com>
2025-02-09 00:13:59 +0900

    refactor: Combine CORS and file upload configurations into AppConfig

--- dollog/src/main/java/com/doldev/dollog/global/config/infra/AppConfig.java
+++ dollog/src/main/java/com/doldev/dollog/global/config/infra/AppConfig.java
@@ -2,12 +2,15 @@ package com.doldev.dollog.global.config.infra;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     CorsFilter corsFilter() {
@@ -15,14 +18,18 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowCredentials(true);
-
         configuration.addAllowedOrigin("http://localhost:3000"); // local에서 react를 사용할 예정
-
         configuration.addAllowedHeader("*"); // 모든 헤더 요청 허용
         configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
         configuration.setMaxAge(3600L); // 1시간 동안 캐싱
         source.registerCorsConfiguration("/**", configuration);
 
         return new CorsFilter(source);
+    }
+
+    @Override
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:./uploads/");
     }
 }
