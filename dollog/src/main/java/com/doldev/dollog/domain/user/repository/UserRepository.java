af45d7fcc4df9d19ae96be5d799521d418e59a85
dol-Dev <dol.coder@gmail.com>
2025-02-09 00:06:08 +0900

    feat: Add existence check methods for username, email, nickname

--- dollog/src/main/java/com/doldev/dollog/domain/user/repository/UserRepository.java
+++ dollog/src/main/java/com/doldev/dollog/domain/user/repository/UserRepository.java
@@ -10,4 +10,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
 
+    boolean existsByUsername(String username);
+
+    boolean existsByNickname(String nickname);
+
+    boolean existsByEmail(String email);
 }
