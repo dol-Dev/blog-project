ad77eb074a575822ed095665d2f27b48efcd4954
dol-Dev <dol.coder@gmail.com>
2025-02-09 23:31:51 +0900

    refactor: Change the return type of the methods to Map<String, String>

--- dollog/src/main/java/com/doldev/dollog/global/auth/service/TokenManager.java
+++ dollog/src/main/java/com/doldev/dollog/global/auth/service/TokenManager.java
@@ -1,5 +1,6 @@
 package com.doldev.dollog.global.auth.service;
 
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -17,41 +18,48 @@ public class TokenManager {
     @Value("${jwt.token.refresh-expiration-time}")
     private long refreshExpirationTime;
 
-    public String generateTokens(String username) {
-        // 토큰 생성
+    // 새로운 액세스 토큰과 리프레시 토큰을 생성하고, 리프레시 토큰을 Redis에 저장한 후 반환해
+    public Map<String, String> generateTokens(String username) {
         String accessToken = jwtProvider.createAccessToken(username);
         String refreshToken = jwtProvider.createRefreshToken(username);
 
-        // Redis에 Refresh 저장
-        redisTemplate.opsForValue().set("refresh_" + username, refreshToken, refreshExpirationTime,
-                TimeUnit.MILLISECONDS);
+        redisTemplate.opsForValue().set(
+            "refresh_" + username,
+            refreshToken,
+            refreshExpirationTime,
+            TimeUnit.MILLISECONDS
+        );
 
-        return accessToken;
+        return Map.of(
+            "accessToken", accessToken,
+            "refreshToken", refreshToken
+        );
     }
 
-
-    public String generateNewTokens(String username) {
+    // 기존 리프레시 토큰을 사용하여 새로운 액세스 토큰과 리프레시 토큰을 생성하고, 기존 리프레시 토큰을 삭제한 후 새로 생성된 토큰들을 Redis에 저장하며 반환해
+    public Map<String, String> generateNewTokens(String username) {
         String refreshTokenKey = "refresh_" + username;
 
-        // 1. 기존 리프레시 토큰 한번 사용 후 삭제
         String existingRefreshToken = redisTemplate.opsForValue().get(refreshTokenKey);
         if (existingRefreshToken == null) {
-            throw new IllegalStateException("No refresh token available");
+            throw new IllegalStateException("사용 가능한 리프레시 토큰이 없어");
         }
+
         redisTemplate.delete(refreshTokenKey);
 
-        // 2. 새로운 토큰 쌍 생성
         String newAccessToken = jwtProvider.createAccessToken(username);
         String newRefreshToken = jwtProvider.createRefreshToken(username);
 
-        // 3. 새 리프레시 토큰 저장
         redisTemplate.opsForValue().set(
-                refreshTokenKey,
-                newRefreshToken,
-                refreshExpirationTime,
-                TimeUnit.MILLISECONDS
-                );
+            refreshTokenKey,
+            newRefreshToken,
+            refreshExpirationTime,
+            TimeUnit.MILLISECONDS
+        );
 
-        return newAccessToken;
+        return Map.of(
+            "accessToken", newAccessToken,
+            "refreshToken", newRefreshToken
+        );
     }
 }
