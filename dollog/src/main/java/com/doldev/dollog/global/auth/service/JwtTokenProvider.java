704b494df1a73a0f1122a6300c82fb2858dd9151
dol-Dev <dol.coder@gmail.com>
2025-02-09 23:56:57 +0900

    refactor: Remove Redis storage and simplify token handling

--- dollog/src/main/java/com/doldev/dollog/global/auth/service/JwtTokenProvider.java
+++ dollog/src/main/java/com/doldev/dollog/global/auth/service/JwtTokenProvider.java
@@ -3,10 +3,8 @@ package com.doldev.dollog.global.auth.service;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -26,21 +24,19 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class JwtTokenProvider {
 
-    private final RedisTemplate<String, String> redisTemplate;
-
     // Header KEY 값
     public static final String AUTHORIZATION_HEADER = "Authorization";
 
     // Token 식별자
     public static final String BEARER_PREFIX = "Bearer ";
 
-    @Value("${jwt.token.access-expiration-time}")
-    private long accessExpirationTime;
+    @Value("${jwt.access.expiration}")
+    private long accessExpireDate;
 
-    @Value("${jwt.token.refresh-expiration-time}")
-    private long refreshExpirationTime;
+    @Value("${jwt.refresh.expiration}")
+    private long refreshExpireDate;
 
-    // 어플리케이션에서 유일한 SecretKey를 사용하고, 해당 SecretKey는 Base64형식으로 인코딩되어 있음.
+    // 애플리케이션에서 유일한 SecretKey를 사용하고, 해당 SecretKey는 Base64형식으로 인코딩되어 있음.
     @Value("${jwt.secret.key}")
     private String secretKey;
 
@@ -49,6 +45,7 @@ public class JwtTokenProvider {
 
     @PostConstruct
     public void init() {
+
         byte[] bytes = Base64.getDecoder().decode(secretKey);
         key = Keys.hmacShaKeyFor(bytes);
     }
@@ -56,11 +53,11 @@ public class JwtTokenProvider {
     // 액세스 토큰 생성
     public String createAccessToken(String username) {
         Date now = new Date();
-        Date expireDate = new Date(now.getTime() + accessExpirationTime);
+        Date expireDate = new Date(now.getTime() + accessExpireDate);
 
         return BEARER_PREFIX +
                 Jwts.builder()
-                        .setSubject(username) // Subject Claim
+                        .setSubject(username)
                         .setIssuedAt(now) // 발급일
                         .setExpiration(expireDate) // 만료일
                         .signWith(key, SignatureAlgorithm.HS256)
@@ -70,22 +67,15 @@ public class JwtTokenProvider {
     // 리프레시 토큰 생성
     public String createRefreshToken(String username) {
         Date now = new Date();
-        Date expireDate = new Date(now.getTime() + refreshExpirationTime);
+        Date expireDate = new Date(now.getTime() + refreshExpireDate);
 
-        String refreshToken = BEARER_PREFIX +
+        return BEARER_PREFIX +
                 Jwts.builder()
                         .setIssuedAt(now)
                         .setExpiration(expireDate)
                         .signWith(key, SignatureAlgorithm.HS256)
                         .compact();
 
-        // redis에 저장
-        redisTemplate.opsForValue().set(
-                "refresh_" + username,
-                refreshToken,
-                refreshExpirationTime,
-                TimeUnit.MILLISECONDS);
-        return refreshToken;
     }
 
     // JWT 토큰 substring
@@ -115,23 +105,6 @@ public class JwtTokenProvider {
 
     // Jwt토큰에서 Claim(사용자 정보)를 추출
     public Claims getUserInfoFromToken(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-    // 만료된 토큰에서 클레임 추출
-    public Claims getClaimsFromExpiredToken(String token) throws ExpiredJwtException {
-        try {
-            return Jwts.parserBuilder()
-                    .setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-        } catch (ExpiredJwtException e) {
-            return e.getClaims(); // 만료된 경우 클레임 반환
-        }
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
     }
 }
