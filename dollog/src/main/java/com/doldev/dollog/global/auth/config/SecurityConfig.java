76bd5efe1d4a532fb916c483080d41dcb3abe52d
dol-Dev <dol.coder@gmail.com>
2025-02-10 00:25:32 +0900

    refactor: add custom authentication and login filters, update security configuration

--- dollog/src/main/java/com/doldev/dollog/global/auth/config/SecurityConfig.java
+++ dollog/src/main/java/com/doldev/dollog/global/auth/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.doldev.dollog.global.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,13 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
+import com.doldev.dollog.global.auth.filter.CustomAuthenticationFilter;
+import com.doldev.dollog.global.auth.filter.CustomLoginFilter;
+import com.doldev.dollog.global.auth.service.CustomUserDetailsService;
+import com.doldev.dollog.global.auth.service.JwtTokenProvider;
+import com.doldev.dollog.global.auth.service.TokenManager;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -19,26 +27,47 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtUtil;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final TokenManager tokenManager;
+    private final CustomUserDetailsService customUserDetailsService;
     private final CorsFilter corsFilter;
-
-    @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+    private final RedisTemplate<String, String> redisTemplate;
+    private final ObjectMapper objectMapper;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         // 정책 및 필터 순서 설정
         http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(customAuthenticationFilter(), CustomLoginFilter.class)
+                .addFilterBefore(customLoginFilter(), UsernamePasswordAuthenticationFilter.class); // CustomAuthenticationFilter
 
         // 인가 설정
         http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.anyRequest().permitAll());
 
         return http.build();
+    }
+
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    private CustomLoginFilter customLoginFilter() throws Exception {
+        CustomLoginFilter filter = new CustomLoginFilter(tokenManager, objectMapper);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        filter.setFilterProcessesUrl("/api/auth/login"); // Url Mapping
+        return filter;
+    }
+
+    private CustomAuthenticationFilter customAuthenticationFilter() {
+        return new CustomAuthenticationFilter(jwtUtil, tokenManager, customUserDetailsService, redisTemplate,
+                objectMapper);
     }
 }
