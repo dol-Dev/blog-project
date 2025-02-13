2fbaa019a21127f787e16e8e881f758fee7cd7d2
dol-Dev <dol.coder@gmail.com>
2025-02-14 03:30:04 +0900

    refactor: remove CustomLoginFilter and update SecurityConfig to use new authentication mechanisms

--- dollog/src/main/java/com/doldev/dollog/global/auth/config/SecurityConfig.java
+++ dollog/src/main/java/com/doldev/dollog/global/auth/config/SecurityConfig.java
@@ -2,9 +2,6 @@ package com.doldev.dollog.global.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,12 +10,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
-import com.doldev.dollog.domain.account.snsUser.application.CustomOAuth2UserService;
+import com.doldev.dollog.domain.account.snsUser.service.CustomOAuth2UserService;
 import com.doldev.dollog.global.auth.filter.CustomAuthenticationFilter;
-import com.doldev.dollog.global.auth.filter.CustomLoginFilter;
-import com.doldev.dollog.global.auth.service.CustomUserDetailsService;
-import com.doldev.dollog.global.auth.service.JwtTokenProvider;
-import com.doldev.dollog.global.auth.service.TokenManager;
+import com.doldev.dollog.global.auth.service.CookieManager;
+import com.doldev.dollog.global.auth.service.TokenAuthenticationManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -28,53 +23,34 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtTokenProvider jwtUtil;
-    private final AuthenticationConfiguration authenticationConfiguration;
-    private final TokenManager tokenManager;
-    private final CustomUserDetailsService customUserDetailsService;
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CorsFilter corsFilter;
-    private final RedisTemplate<String, String> redisTemplate;
     private final ObjectMapper objectMapper;
+    private final TokenAuthenticationManager tokenAuthenticationManager;
+    private final CookieManager cookieManager;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        // 정책 및 필터 순서 설정
-        http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(
-                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(customAuthenticationFilter(), CustomLoginFilter.class)
-                .addFilterBefore(customLoginFilter(), UsernamePasswordAuthenticationFilter.class); // CustomAuthenticationFilter
-
-        // 인가 설정
-        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.anyRequest().permitAll());
-
-        // OAuth2 설정
-        http.oauth2Login(oauth2Login -> oauth2Login
-                .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(customOAuth2UserService))
-                .successHandler((request, response, authentication) -> response.sendRedirect("http://localhost:3000"))); // 추후 리액트 주소로 변경 
+                .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+                        .successHandler((req, res, auth) -> res.sendRedirect("http://localhost:3000")));
 
         return http.build();
     }
 
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    private CustomLoginFilter customLoginFilter() throws Exception {
-        CustomLoginFilter filter = new CustomLoginFilter(tokenManager, objectMapper);
-        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
-        filter.setFilterProcessesUrl("/api/auth/login"); // Url Mapping
-        return filter;
-    }
-
     private CustomAuthenticationFilter customAuthenticationFilter() {
-        return new CustomAuthenticationFilter(jwtUtil, tokenManager, customUserDetailsService, redisTemplate,
-                objectMapper);
+        return CustomAuthenticationFilter.builder()
+                .tokenAuthenticationManager(tokenAuthenticationManager)
+                .cookieManager(cookieManager)
+                .objectMapper(objectMapper)
+                .build();
     }
 }
