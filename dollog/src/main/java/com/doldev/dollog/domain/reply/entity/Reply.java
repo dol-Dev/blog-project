3f7b65b6a81508bfc59d9a5ef574e47c43458e07
dol-Dev <dol.coder@gmail.com>
2025-02-14 04:58:14 +0900

    feat: add Reply entity with fields for content, like count, and relationships to Post and User

---
+++ dollog/src/main/java/com/doldev/dollog/domain/reply/entity/Reply.java
@@ -0,0 +1,52 @@
+package com.doldev.dollog.domain.reply.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.doldev.dollog.domain.account.user.entity.User;
+import com.doldev.dollog.domain.post.entity.Post;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ColumnDefault("0")
+    private Long likeCnt;
+
+    @CreationTimestamp
+    private LocalDateTime createDate;
+
+    @UpdateTimestamp
+    private LocalDateTime modifyDate;
+}
