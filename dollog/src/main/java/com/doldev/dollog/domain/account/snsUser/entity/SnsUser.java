4147fa7984854c3575e1580914a1fd6e500e9268
dol-Dev <dol.coder@gmail.com>
2025-02-18 20:47:25 +0900

    refactor: remove unused setter from SnsUser entity

--- dollog/src/main/java/com/doldev/dollog/domain/account/snsUser/entity/SnsUser.java
+++ dollog/src/main/java/com/doldev/dollog/domain/account/snsUser/entity/SnsUser.java
@@ -24,11 +24,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Builder
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
