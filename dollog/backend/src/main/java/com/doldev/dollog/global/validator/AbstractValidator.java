35f5e80689301570068cc7ee3121750cda1f5cc2
dol-Dev <dol.coder@gmail.com>
2025-02-24 03:45:14 +0900

    refactor: remove @NonNull annotations from AbstractValidator methods

--- dollog/backend/src/main/java/com/doldev/dollog/global/validator/AbstractValidator.java
+++ dollog/backend/src/main/java/com/doldev/dollog/global/validator/AbstractValidator.java
@@ -1,6 +1,5 @@
 package com.doldev.dollog.global.validator;
 
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -10,14 +9,14 @@ public abstract class AbstractValidator<T> implements Validator {
 
     // Validator가 주어진 클래스에 대한 유효성 검사를 지원하는지 확인함.
     @Override
-    public boolean supports(@NonNull Class<?> clazz) {
+    public boolean supports(Class<?> clazz) {
         return true;
     }
 
     // 유효성 검사 진행
     @SuppressWarnings("unchecked")
     @Override
-    public void validate(@NonNull Object target, @NonNull Errors errors) {
+    public void validate(Object target, Errors errors) {
         try {
             doValidate((T) target, errors);
         } catch (RuntimeException e) {
@@ -26,7 +25,7 @@ public abstract class AbstractValidator<T> implements Validator {
     }
 
     @Override
-    public @NonNull Errors validateObject(@NonNull Object target) {
+    public Errors validateObject(Object target) {
         return Validator.super.validateObject(target);
     }
 
