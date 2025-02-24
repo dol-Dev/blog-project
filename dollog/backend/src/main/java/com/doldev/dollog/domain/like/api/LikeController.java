d07bf8d9bc8e54a7eed5fa9687b8b4677c1ef439
dol-Dev <dol.coder@gmail.com>
2025-02-24 17:17:20 +0900

    feat: update LikeController to use RequestMapping for API endpoint organization

--- dollog/backend/src/main/java/com/doldev/dollog/domain/like/api/LikeController.java
+++ dollog/backend/src/main/java/com/doldev/dollog/domain/like/api/LikeController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.doldev.dollog.domain.like.application.LikeService;
@@ -13,13 +14,14 @@ import com.doldev.dollog.global.dto.ApiResDto;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@RestController("/api/likes")
+@RestController
+@RequestMapping("/api/likes")
 public class LikeController {
 
     private final LikeService likeService;
 
     // 게시물 좋아요 추가/취소
-    @PostMapping("/posts/{postId}/like")
+    @PostMapping("/posts/{postId}")
     public ResponseEntity<ApiResDto<Void>> togglePostLike(@PathVariable("postId") int postId,
             @AuthenticationPrincipal CustomUserDetails userDetails) {
         likeService.togglePostLike(postId, userDetails.getUser());
@@ -28,7 +30,7 @@ public class LikeController {
     }
 
     // 댓글 및 답글 좋아요 추가/취소
-    @PostMapping("/comments/{commentId}/like")
+    @PostMapping("/comments/{commentId}")
     public ResponseEntity<ApiResDto<Void>> toggleCommentLike(@PathVariable("commentId") int commentId,
             @AuthenticationPrincipal CustomUserDetails userDetails) {
         likeService.toggleCommentLike(commentId, userDetails.getUser());
