6c25eb67955120e2aa3b57c170a7c40d9965decf
dol-Dev <dol.coder@gmail.com>
2025-02-25 00:42:54 +0900

    feat: add PostResDto class for post response data transfer with entity mapping

---
+++ dollog/backend/src/main/java/com/doldev/dollog/domain/post/dto/res/PostResDto.java
@@ -0,0 +1,33 @@
+package com.doldev.dollog.domain.post.dto.res;
+
+import java.time.LocalDateTime;
+
+import com.doldev.dollog.domain.category.entity.Category;
+import com.doldev.dollog.domain.post.entity.Post;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PostResDto {
+    private int id;
+    private String title;
+    private String content;
+    private String nickname;
+    private LocalDateTime createDate;
+    private Category category;
+    private int likeCnt;
+
+    public static PostResDto fromEntity(Post post) {
+        return PostResDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .likeCnt(post.getLikeCnt())
+                .createDate(post.getCreateDate())
+                .nickname(post.getUser().getProfile().getNickname())
+                .category(post.getCategory() != null ? post.getCategory() : null)
+                .build();
+    }
+}
