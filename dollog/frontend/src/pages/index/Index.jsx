377bfdeb8bfe346338226a3d9b0538f02df51ddd
dol-Dev <dol.coder@gmail.com>
2025-02-25 00:47:29 +0900

    feat: implement loading notification and enhance post display with pagination styles

--- dollog/frontend/src/pages/index/Index.jsx
+++ dollog/frontend/src/pages/index/Index.jsx
@@ -6,6 +6,7 @@ import 'semantic-ui-css/semantic.min.css';
 import { Container, Divider, Icon, Item, Pagination } from 'semantic-ui-react';
 import axiosInstance from '../../utils/axiosInstance';
 import styles from './index.module.css';
+import { toast } from "react-toastify";
 
 const DEFAULT_THUMBNAIL = 'https://i.namu.wiki/i/_FIKQ7NQtBilT8QtmXWvjY8FfusWX6uYHmoDPsK70tP_vijKovxuPJrT-oEEdhjlXPRCEJy0zR30MwQpVRQ0WA.webp';
 
@@ -19,8 +20,6 @@ const Index = () => {
         const fetchPosts = async (page) => {
             try {
                 const response = await axiosInstance.get(`http://localhost:8080/api/posts?page=${page}`);
-
-                console.log("📢 API 응답 데이터:", response.data.data); 
 
                 setPosts(response.data.data.content);
                 setTotalPages(response.data.data.totalPages);
@@ -43,7 +42,7 @@ const Index = () => {
     };
 
     if (!posts) {
-        return <div>Loading...</div>;
+        return toast.info("Loading...");
     }
 
     const getThumbnailAndText = (content) => {
@@ -63,71 +62,71 @@ const Index = () => {
     };
 
     return (
-            <Container>
-                <Item.Group className={styles['item-group']}>
-                    {posts.map((post) => {
-                        const { imgSrc, textContent } = getThumbnailAndText(post.content);
-                        return (
-                            <React.Fragment key={post.id}>
-                                <Item
-                                    onClick={() => navigate(`/detail-post/${post.id}`)}
-                                >
-                                    {imgSrc ? (
-                                        <img
-                                            src={imgSrc}
-                                            className={styles['thumbnail']}
-                                            alt="thumbnail"
-                                        />
-                                    ) : (
-                                        <img
-                                            src="/default-thumbnail.jpg"
-                                            className={styles['thumbnail']}
-                                            alt="default thumbnail"
-                                        />
-                                    )}
-                                    <Item.Content>
-                                        <Item.Header>
-                                            {post.title}
-                                        </Item.Header>
-                                        <Item.Description>
-                                            {textContent.length >= 15 ? `${textContent.substring(0, 14)}...` : textContent}
-                                        </Item.Description>
-                                        <Item.Meta>
-                                            <span className={styles['author']}>
-                                                작성자 : {post.nickname || '알 수 없음'}
-                                            </span>
-                                            <span className={styles['meta-separator']}>|</span>
-                                            <span>
-                                                작성일 : {new Date(post.createDate).toLocaleDateString('ko-KR', {
-                                                    year: 'numeric',
-                                                    month: 'short',
-                                                    day: 'numeric',
-                                                    hour: 'numeric',
-                                                    minute: 'numeric',
-                                                })}
-                                            </span>
-                                        </Item.Meta>
-                                    </Item.Content>
-                                </Item>
-                                <Divider className={styles['divider']} />
-                            </React.Fragment>
-                        );
-                    })}
-                </Item.Group>
-                
-                <Container className={styles['pagination-container']}>
-                    <Pagination
-                        defaultActivePage={currentPage + 1}
-                        totalPages={totalPages}
-                        onPageChange={(e, { activePage }) => handlePageClick(activePage - 1)}
-                        ellipsisItem={{ content: <Icon name="ellipsis horizontal" />, icon: true }}
-                        firstItem={{ content: <Icon name="angle double left" />, icon: true }}
-                        lastItem={{ content: <Icon name="angle double right" />, icon: true }}
-                        prevItem={{ content: <Icon name="angle left" />, icon: true }}
-                        nextItem={{ content: <Icon name="angle right" />, icon: true }}
-                    />
-                </Container>
+        <Container>
+            <Item.Group className={styles['item-group']}>
+                {posts.map((post) => {
+                    const { imgSrc, textContent } = getThumbnailAndText(post.content);
+                    return (
+                        <React.Fragment key={post.id}>
+                            <Item
+                                onClick={() => navigate(`/detail-post/${post.id}`)}
+                            >
+                                {imgSrc ? (
+                                    <img
+                                        src={imgSrc}
+                                        className={styles['thumbnail']}
+                                        alt="thumbnail"
+                                    />
+                                ) : (
+                                    <img
+                                        src="/default-thumbnail.jpg"
+                                        className={styles['thumbnail']}
+                                        alt="default thumbnail"
+                                    />
+                                )}
+                                <Item.Content>
+                                    <Item.Header>
+                                        {post.title}
+                                    </Item.Header>
+                                    <Item.Description>
+                                        {textContent.length >= 15 ? `${textContent.substring(0, 14)}...` : textContent}
+                                    </Item.Description>
+                                    <Item.Meta>
+                                        <span className={styles['author']}>
+                                            작성자 : {post.nickname || '알 수 없음'}
+                                        </span>
+                                        <span className={styles['meta-separator']}>|</span>
+                                        <span>
+                                            작성일 : {new Date(post.createDate).toLocaleDateString('ko-KR', {
+                                                year: 'numeric',
+                                                month: 'short',
+                                                day: 'numeric',
+                                                hour: 'numeric',
+                                                minute: 'numeric',
+                                            })}
+                                        </span>
+                                    </Item.Meta>
+                                </Item.Content>
+                            </Item>
+                            <Divider className={styles['divider']} />
+                        </React.Fragment>
+                    );
+                })}
+            </Item.Group>
+
+            <Container className={styles['pagination-container']}>
+                <Pagination
+                    defaultActivePage={currentPage + 1}
+                    totalPages={totalPages}
+                    onPageChange={(e, { activePage }) => handlePageClick(activePage - 1)}
+                    ellipsisItem={{ content: <Icon name="ellipsis horizontal" />, icon: true }}
+                    firstItem={{ content: <Icon name="angle double left" />, icon: true }}
+                    lastItem={{ content: <Icon name="angle double right" />, icon: true }}
+                    prevItem={{ content: <Icon name="angle left" />, icon: true }}
+                    nextItem={{ content: <Icon name="angle right" />, icon: true }}
+                />
             </Container>
+        </Container>
     );
 };
 
