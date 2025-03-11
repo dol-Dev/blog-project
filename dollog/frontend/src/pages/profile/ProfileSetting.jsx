29c2b9cb967eb6d3c7e1719c2d062328ded17e10
dol-Dev <dol.coder@gmail.com>
2025-03-11 20:47:05 +0900

    feat: add user settings page with email and password update functionality and corresponding styles

---
+++ dollog/frontend/src/pages/profile/ProfileSetting.jsx
@@ -0,0 +1,178 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
+import { useAuth } from '../../contexts/AuthContext';
+import AVATAR_URL from '../../utils/avatarUrl';
+import axiosInstance from '../../utils/axiosInstance';
+import styles from './profileSetting.module.css'; // 모듈 CSS 임포트
+
+const ProfileSetting = () => {
+    const navigate = useNavigate();
+    const [nickname, setNickname] = useState('');
+    const [avatar, setAvatar] = useState('');
+    const [blogName, setBlogName] = useState('');
+    const [selectedAvatar, setSelectedAvatar] = useState(null);
+    const { authInfo } = useAuth();
+
+    const [errorState, setErrorState] = useState({
+        nickname: '',
+        blogName: '',
+        avatar: ''
+    });
+
+    useEffect(() => {
+        if (authInfo) {
+            const { nickname, avatarImageName, blogName } = authInfo;
+            setAvatar(AVATAR_URL + avatarImageName);
+            setNickname(nickname);
+            setBlogName(blogName);
+        }
+    }, [authInfo]);
+
+    // 아바타 업데이트
+    const updateAvatar = async (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            // 미리보기 설정 (Data URL)
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setSelectedAvatar(reader.result);
+            };
+            reader.readAsDataURL(file);
+
+            // FormData 객체 생성 및 파일 추가
+            const formData = new FormData();
+            formData.append('avatarFile', file);
+
+            setErrorState({ ...errorState, avatar: '' });
+            try {
+                const response = await axiosInstance.put('/api/profiles/avatar', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                });
+                if (response.status === 200) {
+                    // navigate('/profile');
+                    toast.success("수정 성공.");
+                }
+            } catch (error) {
+                if (error.response) {
+                    const errors = error.response.data.errors || {};
+                    setErrorState((prevState) => ({
+                        ...prevState,
+                        avatar: errors.avatarFile || ''
+                    }));
+                }
+                toast.error("수정 실패. 다시 시도해주세요.");
+            }
+        }
+    };
+
+    // 닉네임 업데이트
+    const updateNickname = async () => {
+        setErrorState({ ...errorState, nickname: '' });
+        try {
+            const response = await axiosInstance.put('/api/profiles/nickname', {
+                newNickname: nickname
+            });
+            if (response === 200) {
+                navigate('/');
+            }
+        } catch (error) {
+            if (error.response) {
+                const errors = error.response.data.errors || {};
+                setErrorState((prevState) => ({
+                    ...prevState,
+                    nickname: errors.newNickname || ''
+                }));
+            }
+
+            toast.error("수정 실패. 다시 시도해주세요.");
+        }
+    };
+
+    // 블로그이름 업데이트
+    const updateBlogName = async () => {
+        setErrorState({ ...errorState, blogName: '' });
+        try {
+            const response = await axiosInstance.put('/api/profiles/blogName', {
+                newBlogName: blogName
+            });
+            if (response === 200) {
+                navigate('/');
+            }
+        } catch (error) {
+            if (error.response) {
+                const errors = error.response.data.errors || {};
+                setErrorState((prevState) => ({
+                    ...prevState,
+                    blogName: errors.newBlogName || ''
+                }));
+            }
+            toast.error("수정 실패. 다시 시도해주세요.");
+        }
+    };
+
+    return (
+        <>
+            <br /><br />
+            <div className={`container ${styles.containerStyle}`}>
+                <div className={styles.profileContainer}>
+                    {/* 아바타 이미지 */}
+                    <div className={styles.avatarContainer}>
+                        <img
+                            src={selectedAvatar || avatar}
+                            alt="Profile Avatar"
+                            className={styles.avatarImage}
+                        />
+                        {/* 호버 시 나타나는 오버레이 */}
+                        <div
+                            className={styles.avatarOverlay}
+                            onClick={() => document.getElementById('avatar-input').click()}
+                        >
+                            아바타 수정
+                        </div>
+                        <input
+                            id="avatar-input"
+                            type="file"
+                            accept="image/*"
+                            style={{ display: 'none' }}
+                            onChange={updateAvatar}
+                        />
+                        {errorState.avatar && <span className={styles.errorMessage}>{errorState.avatar}</span>}
+                    </div>
+
+                    {/* 프로필 입력 폼 */}
+                    <div className={styles.profileInput}>
+                        <div className={styles.inputGroup}>
+                            <label>닉네임</label>
+                            <input
+                                type="text"
+                                placeholder="닉네임을 입력하세요"
+                                value={nickname}
+                                onChange={(e) => setNickname(e.target.value)}
+                            />
+                            {errorState.nickname && <span className={styles.errorMessage}>{errorState.nickname}</span>}
+                            <button type="button" onClick={updateNickname}>수정</button>
+                        </div>
+
+                        <div className={styles.inputGroup}>
+                            <label>블로그 이름</label>
+                            <input
+                                type="text"
+                                placeholder="블로그 이름을 입력하세요"
+                                value={blogName}
+                                onChange={(e) => setBlogName(e.target.value)}
+                            />
+                            {errorState.blogName && <span className={styles.errorMessage}>{errorState.blogName}</span>}
+                            <button type="button" onClick={updateBlogName}>수정</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ProfileSetting;
---
+++ dollog/frontend/src/pages/user/info/UserSetting.jsx
@@ -0,0 +1,156 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { useAuth } from '../../../contexts/AuthContext';
+import styles from './userSetting.module.css'; // 모듈 CSS 임포트
+import axiosInstance from '../../../utils/axiosInstance';
+
+const UserSetting = () => {
+    const [username, setUsername] = useState('');
+    const [newEmail, setNewEmail] = useState('');
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmNewPassword, setConfirmNewPassword] = useState(''); // 새 비밀번호 확인
+    const { authInfo } = useAuth();
+
+    const [errorState, setErrorState] = useState({
+        newEmail: '',
+        currentPassword: '',
+        newPassword: ''
+    });
+
+    useEffect(() => {
+        if (authInfo) {
+            setNewEmail(authInfo.email)
+            setUsername(authInfo.username)
+
+        }
+    }, [authInfo]);
+
+
+    // 이메일 업데이트
+    const updateEmail = async () => {
+        setErrorState({ ...errorState, newEmail: '' });
+        try {
+            const response = await axiosInstance.put('/api/users/email', {
+                newEmail: newEmail
+            });
+            if (response === 200) {
+                // navigate('/');
+            }
+        } catch (error) {
+            if (error.response) {
+                const errors = error.response.data.errors || {};
+                setErrorState((prevState) => ({
+                    ...prevState,
+                    newEmail: errors.newEmail || ''
+                }));
+            }
+            toast.error("수정 실패. 다시 시도해주세요.");
+        }
+    };
+
+    // 비밀번호 업데이트
+    const updatePassword = async () => {
+        setErrorState({ ...errorState, currentPassword: '', newPassword: '' });
+        try {
+            if (newPassword && newPassword !== confirmNewPassword) {
+                setErrorState({ ...errorState, newPassword: '새 비밀번호가 일치하지 않습니다.' });
+                toast.error("수정 실패. 다시 시도해주세요.");
+                return;
+            }
+            const response = await axiosInstance.put('/api/users/password', {
+                currentPassword: currentPassword,
+                newPassword: newPassword,
+                username: username
+            });
+            if (response === 200) {
+                // navigate('/');
+            }
+        } catch (error) {
+            if (error.response) {
+                const errors = error.response.data.errors || {};
+                setErrorState((prevState) => ({
+                    ...prevState,
+                    currentPassword: errors.currentPassword || '',
+                    newPassword: errors.newPassword || ''
+                }));
+            }
+            toast.error("수정 실패. 다시 시도해주세요.");
+        }
+    };
+
+    return (
+        <>
+            <br /><br />
+            <div className={`container ${styles.containerStyle}`}>
+                <div className={styles.userContainer}>
+                    {/* 입력 폼 */}
+                    <div className={styles.userInput}>
+
+                        <div className={styles.inputGroup}>
+                            <label>아이디</label>
+                            <input
+                                type="text"
+                                value={username}
+                                className="form-control"
+                                readOnly
+                            />
+                        </div>
+
+                        <div className={styles.inputGroup}>
+                            <label>이메일</label>
+                            <input
+                                type="text"
+                                placeholder="닉네임을 입력하세요"
+                                value={newEmail}
+                                onChange={(e) => setNewEmail(e.target.value)}
+                            />
+                            {errorState.newEmail && <span className={styles.errorMessage}>{errorState.newEmail}</span>}
+                            <button type="button" onClick={updateEmail}>수정</button>
+                        </div>
+
+                        <div className={styles.inputGroup}>
+                            <label>현재 비밀번호</label>
+                            <input
+                                type="password"
+                                placeholder="비밀번호를 바꾸실려면 현재 비밀번호를 입력하세요"
+                                value={currentPassword}
+                                onChange={(e) => setCurrentPassword(e.target.value)}
+                            />
+                            {errorState.currentPassword && <span className={styles.errorMessage}>{errorState.currentPassword}</span>}
+                        </div>
+
+                        {currentPassword && (
+                            <div className={styles.inputGroup}>
+                                <label>새 비밀번호</label>
+                                <input
+                                    type="password"
+                                    placeholder="새 비밀번호를 입력하세요"
+                                    value={newPassword}
+                                    onChange={(e) => setNewPassword(e.target.value)}
+                                />
+                                {errorState.newPassword && <span className={styles.errorMessage}>{errorState.newPassword}</span>}
+                            </div>
+                        )}
+
+                        {newPassword && (
+                            <div className={styles.inputGroup}>
+                                <label>새 비밀번호 확인</label>
+                                <input
+                                    type="password"
+                                    placeholder="변경할 비밀번호를 한번 더 입력하세요"
+                                    value={confirmNewPassword}
+                                    onChange={(e) => setConfirmNewPassword(e.target.value)}
+                                />
+                                <button type="button" onClick={updatePassword}>수정</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default UserSetting;
