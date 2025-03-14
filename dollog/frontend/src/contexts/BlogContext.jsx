ff9e521f7a3e44c885f7f94a7e1acb01060e0862
dol-Dev <dol.coder@gmail.com>
2025-03-14 22:38:21 +0900

    feat: enhance BlogContext to manage sessionStorage for nickname, provider, and blogName

--- dollog/frontend/src/contexts/BlogContext.jsx
+++ dollog/frontend/src/contexts/BlogContext.jsx
@@ -10,29 +10,34 @@ export const BlogProvider = ({ children }) => {
     const [provider, setProvider] = useState(sessionStorage.getItem("provider") || "");
     const [blogName, setBlogName] = useState(sessionStorage.getItem("blogName") || "");
 
+    // 상태를 sessionStorage에 저장 또는 삭제
     useEffect(() => {
         if (nickname) {
             sessionStorage.setItem("nickname", nickname);
+        } else {
+            sessionStorage.removeItem("nickname");
         }
         if (provider) {
             sessionStorage.setItem("provider", provider);
+        } else {
+            sessionStorage.removeItem("provider");
         }
         if (blogName) {
             sessionStorage.setItem("blogName", blogName);
+        } else {
+            sessionStorage.removeItem("blogName");
         }
-    }, [nickname, provider]);
+    }, [nickname, provider, blogName]);
 
     useEffect(() => {
-
-        if (location.pathname === "/posts") {
-            return;
+        const allowedPaths = ['/posts'];
+        // 현재 경로가 allowedPaths 중 하나로 시작하지 않으면 초기화
+        if (!allowedPaths.some(path => location.pathname.startsWith(path))) {
+            setNickname("");
+            setProvider("");
+            setBlogName("");
         }
-
-        sessionStorage.removeItem("nickname");
-        sessionStorage.removeItem("provider");
-        sessionStorage.removeItem("blogName");
     }, [location.pathname]);
-
 
     return (
         <BlogContext.Provider value={{ nickname, provider, blogName, setNickname, setProvider, setBlogName }}>
